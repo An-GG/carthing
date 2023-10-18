4b5de672bc577d395f51a4f0888c31271210267a
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-17 21:31:33 -0500

    interface

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,23 +1,88 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int bufferSize = 100; // buffer size for incoming serial data
+char inputBuffer[bufferSize];
+char* vars[] = {"predicted_lock_state"};
+char predicted_lock_state[bufferSize] = "unknown"; // default state
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("carthing serial interface. type 'help' to get a list of available commands");
+  Serial.print("> ");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  int result = myFunction(2, 3);
-  Serial.print("STATE: ");
-  Serial.println(result);
-  delay(1000);
+  if (Serial.available()) {
+    int len = Serial.readBytesUntil('\n', inputBuffer, bufferSize);
+    inputBuffer[len] = '\0'; // Null-terminate the string
+
+    // Check and handle command
+    if (strcmp(inputBuffer, "help") == 0) {
+      displayHelp();
+    } else if (strcmp(inputBuffer, "unlock") == 0) {
+      // Add your 'unlock' functionality here
+      Serial.println("Unlock command received");
+    } else if (strcmp(inputBuffer, "lock") == 0) {
+      // Add your 'lock' functionality here
+      Serial.println("Lock command received");
+    } else if (strcmp(inputBuffer, "bluetooth_scan") == 0) {
+      // Add your 'bluetooth_scan' functionality here
+      Serial.println("Bluetooth scan started");
+    } else if (strcmp(inputBuffer, "rfid_scan") == 0) {
+      // Add your 'rfid_scan' functionality here
+      Serial.println("RFID scan started");
+    } else if (strncmp(inputBuffer, "get ", 4) == 0) {
+      getVar(inputBuffer + 4);
+    } else if (strncmp(inputBuffer, "set ", 4) == 0) {
+      setVar(inputBuffer + 4);
+    } else if (strcmp(inputBuffer, "list_vars") == 0) {
+      listVars();
+    } else {
+      Serial.println("Unknown command. Type 'help' for available commands.");
+    }
+    Serial.print("> ");
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void displayHelp() {
+  Serial.println("Available commands:");
+  Serial.println("help - Displays this help message");
+  Serial.println("unlock - Unlock command");
+  Serial.println("lock - Lock command");
+  Serial.println("bluetooth_scan - Start a Bluetooth scan");
+  Serial.println("rfid_scan - Start an RFID scan");
+  Serial.println("get [var] - Get value of a variable");
+  Serial.println("set [var] [value] - Set value of a variable");
+  Serial.println("list_vars - List all available variables");
 }
+
+void getVar(char* varName) {
+  if (strcmp(varName, "predicted_lock_state") == 0) {
+    Serial.println(predicted_lock_state);
+  } else {
+    Serial.println("Unknown variable.");
+  }
+}
+
+void setVar(char* command) {
+  char* varName = strtok(command, " ");
+  char* value = strtok(NULL, "");
+
+  if (varName != NULL && value != NULL) {
+    if (strcmp(varName, "predicted_lock_state") == 0) {
+      strncpy(predicted_lock_state, value, sizeof(predicted_lock_state) - 1);
+      Serial.println("Value set.");
+    } else {
+      Serial.println("Unknown variable.");
+    }
+  } else {
+    Serial.println("Invalid set command.");
+  }
+}
+
+void listVars() {
+  for (int i = 0; i < sizeof(vars) / sizeof(vars[0]); i++) {
+    Serial.println(vars[i]);
+  }
+}
+
