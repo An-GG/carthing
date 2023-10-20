aa28539489a71390478737015057a93c1d354f4a
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-20 12:49:35 -0500

    h

--- software/companion-esp32/src/main.cpp
+++ software/companion-esp32/src/main.cpp
@@ -1,8 +1,4 @@
-/*
-   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
-   Ported to Arduino ESP32 by Evandro Copercini
-*/
-
+#include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
