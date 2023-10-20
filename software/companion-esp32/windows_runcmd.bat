e6d571613602164ab360e33ebc005645083ebd4c
An-GG <ankush.girotra@gmail.com>
2023-10-20 13:16:56 -0500

    ino

---
+++ software/companion-esp32/windows_runcmd.bat
@@ -0,0 +1,30 @@
+@echo off
+REM Pull latest changes from git
+git pull
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred during git pull.
+    exit /b %ERRORLEVEL%
+)
+
+
+REM Build and upload using PlatformIO
+C:\Users\ankua\.platformio\penv\Scripts\pio.exe run -t upload
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred during PlatformIO upload.
+    exit /b %ERRORLEVEL%
+)
+
+REM Show git diff
+echo Printing git diff:
+git diff
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred during git diff.
+    exit /b %ERRORLEVEL%
+)
+
+REM Start arduino-cli monitor
+C:\Users\ankua\Desktop\arduino\arduino-cli.exe monitor -p COM5
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred while starting arduino-cli monitor.
+    exit /b %ERRORLEVEL%
+)
