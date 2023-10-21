5e4cfbee9366f94eed1df4c18b1cc2025bdcadd0
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-20 19:38:30 -0500

    remote compile & flash

--- prototyping/espstuff/blscan/pio/run_remote.sh
+++ prototyping/espstuff/blscan/pio/run_remote.sh
@@ -4,4 +4,4 @@ git aac 'remote compile & flash'
 
 git push
 
-ssh pro 'cd ~/Documents/Projects/carthing/prototyping/espstuff/blscan/pio && /usr/local/bin/git pull && ./run.sh'
+ssh pro 'cd ~/Documents/Projects/carthing/prototyping/espstuff/blscan/pio && /usr/local/bin/git pull && /usr/local/bin/pio run -t upload -t compiledb -t monitor'
