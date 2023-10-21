a53e4e29627ca5a8e133609038644ae24b87b8b0
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-20 20:27:32 -0500

    scripts

---
+++ software/arduino-mega/remote_runcmd.sh
@@ -0,0 +1,7 @@
+#/usr/bin/env bash 
+
+#git aac 'remote compile & flash'
+
+#git push
+
+ssh -t pro 'cd ~/Documents/Projects/carthing/software/arduino-mega && /usr/local/bin/git pull && /usr/local/bin/pio run -t upload && PORT="$(ls /dev/cu.usb*)" && echo "PORT=$PORT - Git Diff:" && git diff && /usr/local/bin/arduino-cli monitor -p $PORT'
