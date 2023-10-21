828831740fdd11588f8e2004490f63b515fb7dd4
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-20 20:03:30 -0500

    remote compile & flash

--- prototyping/espstuff/blscan/pio/run.sh
+++ prototyping/espstuff/blscan/pio/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-/usr/local/bin/pio run -t upload -t compiledb 
+/usr/local/bin/pio run -t upload -t compiledb
 
 PORT="$(ls /dev/cu.usb*)" 
 /usr/local/bin/arduino-cli monitor -p $PORT
--- prototyping/espstuff/blscan/pio/run_remote.sh
+++ prototyping/espstuff/blscan/pio/run_remote.sh
@@ -4,4 +4,4 @@ git aac 'remote compile & flash'
 
 git push
 
-ssh -t pro 'cd ~/Documents/Projects/carthing/prototyping/espstuff/blscan/pio && /usr/local/bin/git pull && /usr/local/bin/pio run -t upload -t compiledb && PORT="$(ls /dev/cu.usb*)" && /usr/local/bin/arduino-cli monitor -p $PORT'
+ssh -t pro 'cd ~/Documents/Projects/carthing/prototyping/espstuff/blscan/pio && /usr/local/bin/git pull && /usr/local/bin/pio run -t upload && PORT="$(ls /dev/cu.usb*)" && /usr/local/bin/arduino-cli monitor -p $PORT'
