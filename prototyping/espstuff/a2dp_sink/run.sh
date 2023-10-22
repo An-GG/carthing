2bea214681b6c54524e1f07516954a3c4e11df48
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-21 23:16:52 -0500

    run.sh

--- prototyping/espstuff/a2dp_sink/run.sh
+++ prototyping/espstuff/a2dp_sink/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash 
 
-arduino-cli compile -b esp32:esp32:esp32 blscan.ino
+arduino-cli compile -b esp32:esp32:esp32 ./a2dp_sink.ino
 
 PORT="$(ls /dev/cu.usb*)"
-arduino-cli upload -b esp32:esp32:esp32 -p $PORT blscan.ino
+arduino-cli upload -b esp32:esp32:esp32 -p $PORT ./a2dp_sink.ino
 
 arduino-cli monitor -p $PORT --config baudrate=9600
