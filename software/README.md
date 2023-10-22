235e0f2fe97424de69abfd4bb792c62e5c8fb4b7
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-21 22:44:24 -0500

    progress update

---
+++ software/README.md
@@ -0,0 +1,41 @@
+# part A notes
+
+what we really want is a similar experience to the tesla phone key but without using a custom app that has to run in the background on iOS
+    - to do this with nonzero "security" we can use bluetooth pairing, so if paired device is within certain rssi 
+    - need esp32 to show up in iOS's Bluetooh Settings, need to pretend to be a bluetooth audio device or something
+        - Audio/Video Remote Control Profile (AVRCP) might be ideal if it works
+
+```
+https://github.com/pschatzmann/ESP32-A2DP?tab=readme-ov-file
+
+prototyping/espstuff/a2dp_sink/a2dp_sink.ino
+```
+```
+https://www.visualmicro.com/page/Easy-DIY-Bluetooth-Speaker-Project.aspx
+```
+
+# the project is now very rapidly approaching scuffed and unmaintable code territory 
+
+--- 
+
+alternative:
+abandon pairing, literally just use advertised device name and check rssi
+
+> is this even reliable? even assuming no low power mode nonsense, is this always available?
+
+
+---
+
+# part B notes: required bt app fallback
+
+custom ios app 
+ - two buttons: lock, unlock
+ - easy: connect to esp using uuid, write password + action to esp32 
+ - better: 
+        - use time based rolling code as the password 
+        - do this with bt pairing ? how secure even is this if i dont try
+
+```
+https://github.com/espressif/arduino-esp32/blob/master/libraries/BluetoothSerial/src/BluetoothSerial.h
+```
+
--- software/companion-esp32/src/main.cpp
+++ software/companion-esp32/src/main.cpp
@@ -1,36 +1,68 @@
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include <BLEServer.h>
 
-int scanTime = 5; //In seconds
-BLEScan* pBLEScan;
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.printf("%d  - \t Advertised Device: %s \n", advertisedDevice.getPayload(), advertisedDevice.toString().c_str());
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+class MyCallbacks: public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() > 0)
+    {
+      Serial.println("*********");
+      Serial.print("New value: ");
+      for (int i = 0; i < value.length(); i++)
+      {
+        Serial.print(value[i]);
+      }
+
+      Serial.println();
+      Serial.println("*********");
     }
+  }
 };
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Scanning...");
 
-  BLEDevice::init("");
-  pBLEScan = BLEDevice::getScan(); //create new scan
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
-  pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99);  // less or equal setInterval value
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to ESP32-BLE_Server");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+
+  BLEDevice::init("ESP32-BLE-Server");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-  delay(200);
+void loop()
+{
+  delay(2000);
 }
+
