e9c38b01aa40e3064c3b9b442e0af37c606b0926
Ankush Girotra <ankush.girotra@gmail.com>
2023-10-23 00:43:14 -0500

    xcode-project-init

---
+++ software/simple-ios-keyfob/xcode/carkey-app/carkey-app/App.swift
@@ -0,0 +1,17 @@
+//
+//  carkey_appApp.swift
+//  carkey-app
+//
+//  Created by an on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct carkey_app: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ software/simple-ios-keyfob/xcode/carkey-app/carkey-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  carkey-app
+//
+//  Created by an on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
